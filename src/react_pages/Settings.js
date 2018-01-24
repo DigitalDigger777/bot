e8a3c81adf94d957f19394b6a91fc812db2086b5
korman <korman.yuri@gmail.com>
2018-01-24 16:21:45 +0200

    finished setting

--- src/react_pages/Settings.js
+++ src/react_pages/Settings.js
@@ -6,8 +6,75 @@ import React from 'react';
 import Header from '../react_components/Header';
 import SideBar from '../react_components/SideBar';
 import PageContainer from '../react_components/PageContainer';
+import Config from '../Config';
 
 export default class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const config = new Config();
+
+        this.state = {
+            ask: null,
+            telNotification: null,
+            emailNotification: null,
+            shopper: window.localStorage.getItem('shopper'),
+            baseUrl: config.baseUrl
+        };
+
+        this.changeAsk = this.changeAsk.bind(this);
+        this.changeTelNotification = this.changeTelNotification.bind(this);
+        this.changeEmailNotification = this.changeEmailNotification.bind(this);
+    }
+
+    changeAsk(e) {
+        this.setState({
+            ask: e.target.value
+        });
+    }
+
+    changeTelNotification(e) {
+        this.setState({
+            telNotification: e.target.value
+        });
+    }
+
+    changeEmailNotification(e) {
+        this.setState({
+            emailNotification: e.target.value
+        });
+    }
+
+    save(){
+        console.log(this.state);
+        //redirect to /#/admin/profile
+        axios.post(this.state.baseUrl + 'gift-card/rest/shopper/' + this.state.shopper.id, {
+            ask:                this.state.ask,
+            telNotification:    this.state.telNotification,
+            emailNotification:  this.state.emailNotification
+        })
+            .then(() => {
+                //window.location = '/bot-admin/profile';
+                this.setState({
+                    alert: {
+                        open: true,
+                        message: 'Save successful'
+                    }
+                });
+
+                setTimeout(() => {
+                    this.setState({
+                        alert: {
+                            open: false,
+                            message: ''
+                        }
+                    });
+                }, 3000);
+            })
+            .catch(error => {
+
+            });
+    }
 
     render() {
         return(
@@ -33,6 +100,11 @@ export default class Settings extends React.Component {
                                 <div className="form-group form-group-default required ">
                                     <label>Email for notification</label>
                                     <input type="email" className="form-control" required/>
+                                </div>
+                                <div className="row">
+                                    <div style={{textAlign: 'center'}}>
+                                        <button className="btn btn-primary" onClick={this.save.bind(this)}>Save</button>
+                                    </div>
                                 </div>
                             </form>
                         </div>
